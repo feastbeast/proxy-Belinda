02f35f33820ae7ff0e9630019cdee19d291499ee
OliverHan <ohan88@gmail.comgit config --global user.email ohan88@gmail.com>
2018-04-23 17:12:03 -0700

    First proxy commit.

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+// const morgan = require('morgan');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// app.use(morgan('dev'));
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+
+app.use('/restaurants', express.static(path.join(__dirname, './public')));
+
+app.get('/restaurants/:id', function(req, res) {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+
+
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
+});
