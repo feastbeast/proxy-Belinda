4bcd05282f549ffb4d80e06777079bcd00627397
OliverHan <ohan88@gmail.comgit config --global user.email ohan88@gmail.com>
2018-04-26 18:50:35 -0700

    Built in Docker compose.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 // const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 // app.use(morgan('dev'));
 const bodyParser = require('body-parser');
