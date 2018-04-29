d6618bd606b6b38cf69b3478d5697880f7aaba46
OliverHan <ohan88@gmail.comgit config --global user.email ohan88@gmail.com>
2018-04-28 17:46:30 -0700

    Setup ESlint.

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require('express');
 // const morgan = require('morgan');
 const path = require('path');
+
 const app = express();
 const port = 3000;
 
 // app.use(morgan('dev'));
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 
 app.use('/restaurants', express.static(path.join(__dirname, './public')));
 
-app.get('/restaurants/:id', function(req, res) {
-  res.sendFile(path.join(__dirname, './public/index.html'))
-})
+app.get('/restaurants/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.use(express.static(path.join(__dirname, './public')));
 
