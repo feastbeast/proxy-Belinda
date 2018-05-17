693242a00ede6a1dfe7d30ac23b5dc4b1be929e3
Belinda <belindamomo@gmail.com>
2018-05-16 19:59:24 -0700

    change scripts

--- templates/scripts.js
+++ templates/scripts.js
@@ -11,7 +11,7 @@ module.exports = (items) => `
   <script>
   ${items.map(item => `
   ReactDOM.hydrate(
-    React.createElement(Nearby),
+    React.createElement(Nearby, {restaurantId: window.location.href.split('/restaurants/')[1]}),
     document.getElementById('${item}')
   );`).join('\n')}
   </script>
